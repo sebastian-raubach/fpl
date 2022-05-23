e49abd8fa5a2a090894f89d09f9a7a7df11f53a4
sebastian-raubach <sebastian.raubach@hutton.ac.uk>
2022-05-23 10:35:47 +0100

    - FIX: Relative path issue.

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
   chainWebpack: config => {
     config.module
       .rule('raw')
